079a1dc6077271b94063eeb5c793207c53876a65
Raditya <radityamajid001@gmail.com>
2025-02-17 13:33:56 +0700

    Acong shopping at a grocery store

---
+++ Main Flow.py
@@ -0,0 +1,13 @@
+"""
+Semua Sintakis bahasa pemrograman terdiri dari 3 yaitu:
+1. Sekuensial: Langkah berurutan
+2. Pecabangan: Langkah melompat bila kondisi terpenuhi
+3. Pengulangan: Mengulang langkah bila kondisi terpenuhi
+"""
+
+#sekuensial
+print('Ibu berkata, "pergilah ke warung"')
+print('Acong menjawab, "oke apa yang harus aku lakukan ditoko?"')
+print('Ibu menjawab, "belilah 1 botol susu, dan jika ada telur beli 6"')
+print('maka Acong berangkat ke warung')
+print('Dan berbelanja')
