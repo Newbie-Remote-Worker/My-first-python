91459bf98c034840ec3456801eaf5104a8b8d836
Raditya <radityamajid001@gmail.com>
2025-02-17 12:24:41 +0700

    Demo "Papi Ai"  pake Python

--- Main.py
+++ Main.py
@@ -2,4 +2,5 @@
 Ini adalam demo menggunakan Pyrhon
 """
 print ("Papi Ai punya Villa di Bali")
-print ("Papi Ai punya Lexus di PIK")
+print ("Papi Ai punya Lexus")
+print ("Papi Ai punya Rumah di PIK")
