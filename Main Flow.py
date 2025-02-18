5c6fdb6d982b3fe59f7392d809d0348dbd3439cd
Raditya <radityamajid001@gmail.com>
2025-02-18 20:06:15 +0700

    Acong shopping at a grocery store

--- Main Flow.py
+++ Main Flow.py
@@ -6,8 +6,32 @@ Semua Sintakis bahasa pemrograman terdiri dari 3 yaitu:
 """
 
 #sekuensial
-print('Ibu berkata, "pergilah ke warung"')
-print('Acong menjawab, "oke apa yang harus aku lakukan ditoko?"')
-print('Ibu menjawab, "belilah 1 botol susu, dan jika ada telur beli 6"')
-print('maka Acong berangkat ke warung')
-print('Dan berbelanja')
+print('Mom said, "go to grocery store"')
+print('Acong reply, "oke, what do i do?"')
+print('Mom said, "buy a bottle of milk')
+print('Acong going to grocery store')
+print('Acong searching for the bottle of milk')
+print('Acong checked the price of milk, he had enough money')
+print('Acong searching for the eggs')
+print('Acong checked the price of eggs, he still had enough money')
+print('Acong goes shopping')
+
+# Percabangan
+bottle_of_milk_in_grocery = 20
+eggs_in_grocery = 100
+
+print(f'bottle of milk {bottle_of_milk_in_grocery} btl')
+print(f'eggs in grocecy {eggs_in_grocery} btr')
+
+if bottle_of_milk_in_grocery > 0:
+    print('Acong checked the price of milk, and found that he had enough money. ')
+    if eggs_in_grocery > 0:
+         print('Acong bought 1 btl')
+         print('Acong bought 6 btr')
+    else:
+        print('Acong bought 1 btl')
+        print('Acong did not bought 6 btr')
+else:
+    print('Acong did not bought btl and btr')
+
+print('came home and gave the groceries to his mom')
