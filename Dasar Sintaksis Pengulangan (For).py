9d75aefb2b6fbf5f7e1f5301c2f33f3e113bfd18
Raditya <radityamajid001@gmail.com>
2025-02-18 21:43:35 +0700

    Acong read the books

--- Dasar Sintaksis Pengulangan (For).py
+++ Dasar Sintaksis Pengulangan (For).py
@@ -7,13 +7,13 @@ Semua Sintakis bahasa pemrograman terdiri dari 3 yaitu:
 
 # 3. Pengulangan
 
-jumlah_buku = 100
+all_books = 10
 print('mom said, "read all books"')
 
 read_books = 0
 print(f'read books {read_books}')
 
-for read_books in range(1, 101):
+for read_books in range(1, 11):
     print(f'books {read_books}, read books')
 
 print(f'read books {read_books}')
