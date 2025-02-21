7c68af23ca91e589235634665e6dd9608e3e54b4
Raditya <radityamajid001@gmail.com>
2025-02-21 15:22:16 +0700

    Book list

--- Dasar Sintaksis Data List.py
+++ Dasar Sintaksis Data List.py
@@ -3,7 +3,18 @@ Dasar Data List
 """
 
 books_list = ['Atomic habits','7 Deadly Sins','48 Law of Power']
+print('showing variable books_list')
 print(books_list)
 
+print('proces all with for in')
 for book in books_list:
     print (book)
+
+print('\nShow all books list on some index')
+print(books_list[0])
+print(books_list[1])
+print(books_list[2])
+
+print('\nShow all books list with for in range')
+for n in range(0, len(books_list)):
+    print(books_list[n])
