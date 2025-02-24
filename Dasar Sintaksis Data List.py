86143618e5c12e85a4dcba977b1dcdb90056a191
Raditya <radityamajid001@gmail.com>
2025-02-24 20:31:51 +0700

    Book list

--- Dasar Sintaksis Data List.py
+++ Dasar Sintaksis Data List.py
@@ -16,5 +16,17 @@ print(books_list[1])
 print(books_list[2])
 
 print('\nShow all books list with for in range')
-for n in range(0, len(books_list)):
-    print(books_list[n])
+for i in range(0, len(books_list)):
+    print(books_list[i])
+
+books_list = [2, 'Habit no 1', 212, 'section 2']
+print('\nShow all books list wigh for in range')
+for i in range(1, len(books_list)):
+    print(books_list [i])
+
+print('\nbring back value of books_list')
+books_list = ['Atomic habits','7 Deadly Sins','48 Law of Power']
+print('\nAdd 1 new book')
+books_list.append('Madilog')
+for i in range(0, len(books_list)):
+    print(books_list [i])
