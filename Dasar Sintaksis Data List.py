19b85dd7cb379883c0c75249e5918ecc5cbf810b
Raditya <radityamajid001@gmail.com>
2025-02-27 19:28:20 +0700

    Book list

--- Dasar Sintaksis Data List.py
+++ Dasar Sintaksis Data List.py
@@ -2,7 +2,7 @@
 Dasar Data List
 """
 
-books_list = ['Atomic habits','7 Deadly Sins','48 Law of Power']
+books_list = ['Deadly 7 Sins','Atomic habits','Law of Power']
 print('showing variable books_list')
 print(books_list)
 
@@ -10,23 +10,37 @@ print('proces all with for in')
 for book in books_list:
     print (book)
 
+# Memulai list buku dari urutan yang ditentukan
+
 print('\nShow all books list on some index')
 print(books_list[0])
 print(books_list[1])
 print(books_list[2])
 
 print('\nShow all books list with for in range')
-for i in range(0, len(books_list)):
+for i in range(1, len(books_list)):
     print(books_list[i])
+
+# Menambahkan section pada buku
 
 books_list = [2, 'Habit no 1', 212, 'section 2']
 print('\nShow all books list wigh for in range')
 for i in range(1, len(books_list)):
     print(books_list [i])
 
-print('\nbring back value of books_list')
-books_list = ['Atomic habits','7 Deadly Sins','48 Law of Power']
+# Menambah buku dalam data list
+
+print('\nBring back value of books_list')
+books_list = ['Deadly 7 Sins','Atomic habits','Law of Power']
 print('\nAdd 1 new book')
 books_list.append('Madilog')
 for i in range(0, len(books_list)):
     print(books_list [i])
+
+# Mengganti buku dalam data list
+
+print('\nReplace second book')
+books_list = ['Deadly 7 Sins','Atomic habits','Law of Power']
+books_list [1] = 'Micro habits'
+for i in range(len (books_list)):
+    print(books_list[i])
