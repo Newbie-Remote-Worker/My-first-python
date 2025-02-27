834a4b0a715e4ac35cc917f701c3a76582a13634
Raditya <radityamajid001@gmail.com>
2025-02-27 19:30:05 +0700

    Book list

--- Dasar Sintaksis Data List.py
+++ Dasar Sintaksis Data List.py
@@ -10,7 +10,7 @@ print('proces all with for in')
 for book in books_list:
     print (book)
 
-# Memulai list buku dari urutan yang ditentukan
+# Memulai Elemen dari urutan yang ditentukan
 
 print('\nShow all books list on some index')
 print(books_list[0])
@@ -21,14 +21,14 @@ print('\nShow all books list with for in range')
 for i in range(1, len(books_list)):
     print(books_list[i])
 
-# Menambahkan section pada buku
+# Menambahkan Section Elemen pada Data List
 
 books_list = [2, 'Habit no 1', 212, 'section 2']
 print('\nShow all books list wigh for in range')
 for i in range(1, len(books_list)):
     print(books_list [i])
 
-# Menambah buku dalam data list
+# Menambah Eelemen dalam Data List
 
 print('\nBring back value of books_list')
 books_list = ['Deadly 7 Sins','Atomic habits','Law of Power']
@@ -37,7 +37,7 @@ books_list.append('Madilog')
 for i in range(0, len(books_list)):
     print(books_list [i])
 
-# Mengganti buku dalam data list
+# Mengganti Elemen dalam Data List
 
 print('\nReplace second book')
 books_list = ['Deadly 7 Sins','Atomic habits','Law of Power']
