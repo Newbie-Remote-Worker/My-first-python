83eedab7d0e3529594fdd4ac75895b2570995fd5
Raditya <radityamajid001@gmail.com>
2025-03-02 20:59:38 +0700

    Data List

--- Dasar Sintakis Data List Comperhension.py
+++ Dasar Sintakis Data List Comperhension.py
@@ -24,3 +24,23 @@ del books_list[0:-2] #START:END
 print('Some books have been deleted')
 for i in range(0, len(books_list)):
     print(books_list[i])
+
+print('\nDelete all book of using (Delete Comperhension)')
+books_list = ['Deadly 7 Sins','Atomic habits','Law of Power','Raja Tempe','Madilog','Gerpolek']
+del books_list[:] #START:END
+print('All books have been deleted')
+for i in range(0, len(books_list)):
+    print(books_list[i])
+
+# Membuat Data List baru dengan Comperhension
+
+print('\nMake a new_book_list with Comperhension Ganjil')
+books_list = ['1 Deadly 7 Sins','2 Atomic habits','3 Law of Power','4 Raja Tempe','5 Madilog','6 Gerpolek']
+new_book_list = books_list[0::2]
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
+print('\nMake a new_book_list with Comperhension Genap')
+new_book_list = books_list[1::2]
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
