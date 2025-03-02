30576af2059db16d8fca7cc5a5c2475bd211867e
Raditya <radityamajid001@gmail.com>
2025-03-02 21:43:32 +0700

    JSON

---
+++ Dasara Sintaksis Dictionary.py
@@ -0,0 +1,28 @@
+user = {
+    'id': 1012731,
+    'name': 'Robert Davis Chaniago',
+    'username': 'Robertdavgo',
+    'email': 'robertdago@rocketmail.com',
+    'pass': 'Robertshimself31523',
+    'address': {
+        'street': 'jln. H. Misar',
+        'city': 'Jakarta Timur',
+        'zipcode': '13770',
+        'graphy': {
+            'lat': '-37.3159',
+            'long': '81.1490',
+        }
+    }
+}
+
+print(user)
+print(id)
+print(user['name'])
+print(user['username'])
+print(user['email'])
+print(user['pass'])
+print(user['address'])
+print(user['address']['city'])
+print(user['graphy']['long'])
+
+
